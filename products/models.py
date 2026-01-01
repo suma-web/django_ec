a9467e464f2acd7cb06db0a6f12099cb1ac9aaa6
sample tarou <sample@example.com>
2026-01-01 20:58:24 +0900

    models.pyの編集

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='products/', default='products/image1.jpg', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
