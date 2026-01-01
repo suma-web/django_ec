ac039c3afff6f4f2e56e0893328a74eebe5957f6
sample tarou <sample@example.com>
2026-01-01 20:14:43 +0900

    urls.pyの編集

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', TemplateView.as_view(template_name='hello.html')),
+    path('products/', include('products.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
