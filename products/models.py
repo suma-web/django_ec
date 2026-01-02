a7bc7c517ee30a00e3b414a5f376f3b13bb31c02
sample tarou <sample@example.com>
2026-01-02 17:10:12 +0900

    products/models.pyの修正

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField()
-    image = models.ImageField(upload_to='products/', default='products/image1.jpg', null=True, blank=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
