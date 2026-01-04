e2e4b8bc2dd28e2629836171fc5c36aca56488c2
sample tarou <sample@example.com>
2026-01-04 23:38:10 +0900

    settings.pyにmanages.middleware.BasicAuthMiddlewareを追加”

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "manages.middleware.BasicAuthMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
