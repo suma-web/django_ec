511c3ccd3248bd0b0351831ec1e17bbacd704ef2
sample tarou <sample@example.com>
2026-01-04 23:55:58 +0900

    auth.pyの修正

--- manages/auth.py
+++ manages/auth.py
@@ -1,26 +1,24 @@
-from django.http import HttpResponse
-from functools import wraps
 import base64
+from django.http import HttpResponse
+
 
 def basic_auth_required(view_func):
-    @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        auth_header = request.META.get("HTTP_AUTHORIZATION")
+        auth = request.META.get("HTTP_AUTHORIZATION")
 
-        if auth_header:
+        if auth:
             try:
-                auth_type, encoded = auth_header.split(" ", 1)
-                if auth_type.lower() == "basic":
-                    decoded = base64.b64decode(encoded).decode("utf-8")
+                method, credentials = auth.split(" ", 1)
+                if method.lower() == "basic":
+                    decoded = base64.b64decode(credentials).decode("utf-8")
                     username, password = decoded.split(":", 1)
-
                     if username == "admin" and password == "pw":
                         return view_func(request, *args, **kwargs)
             except Exception:
                 pass
 
-        response = HttpResponse("認証されていません", status=401)
-        response["WWW-Authenticate"] = 'Basic realm="Management Area"'
+        response = HttpResponse("Unauthorized", status=401)
+        response["WWW-Authenticate"] = 'Basic realm="Manage Area"'
         return response
 
     return _wrapped_view
