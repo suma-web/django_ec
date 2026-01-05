731d972d05ee1e5733169a4aed32de1380ea46fd
sample tarou <sample@example.com>
2026-01-05 17:19:27 +0900

    manages/views.pyの編集

--- manages/views.py
+++ manages/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
+from .forms import ProductForm
+from django.views.decorators.http import require_POST
+from products.models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, "manages/product_list.html", {
+        "products": products
+    })
+
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("manages:product_list")
+    else:
+        form = ProductForm()
+
+    return render(request, "manages/product_form.html", {
+        "form": form
+    })
+
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("manages:product_list")
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, "manages/product_form.html", {
+        "form": form,
+        "product": product
+    })
+
+@require_POST
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product.delete()
+    return redirect("manages:product_list")
