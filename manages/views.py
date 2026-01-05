4ee08c7da438554128e3394f003d4b055054af6c
sample tarou <sample@example.com>
2026-01-05 20:26:33 +0900

    @basic_auth_requiredの追加

--- manages/views.py
+++ manages/views.py
@@ -3,14 +3,17 @@ from products.models import Product
 from .forms import ProductForm
 from django.views.decorators.http import require_POST
 from products.models import Product
+from .auth import basic_auth_required
 
 
+@basic_auth_required
 def product_list(request):
     products = Product.objects.all()
     return render(request, "manages/product_list.html", {
         "products": products
     })
 
+@basic_auth_required
 def product_create(request):
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
@@ -24,6 +27,7 @@ def product_create(request):
         "form": form
     })
 
+@basic_auth_required
 def product_edit(request, pk):
     product = get_object_or_404(Product, pk=pk)
 
@@ -40,6 +44,7 @@ def product_edit(request, pk):
         "product": product
     })
 
+@basic_auth_required
 @require_POST
 def product_delete(request, pk):
     product = get_object_or_404(Product, pk=pk)
