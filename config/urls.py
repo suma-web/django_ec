e8f8296a2eb48c68fee8c7ed8cf97dd7259b21d7
sample tarou <sample@example.com>
2026-01-05 17:50:20 +0900

    config/urls.pyの修正

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("products.urls")),
-    path("manage/", include("manage.urls")),
+    path("manage/", include("manages.urls")),
 ]
 
 if settings.DEBUG:
