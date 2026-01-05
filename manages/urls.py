fe50c6547ef2fe057b13f70c89ee16f7456011f3
sample tarou <sample@example.com>
2026-01-05 17:54:10 +0900

    manages/urls.pyの修正

--- manages/urls.py
+++ manages/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = "manages"
 
 urlpatterns = [
-    path("products/", views.product_list),
-    path("products/create/", views.product_create),
-    path("products/<int:pk>/edit/", views.product_edit),
-    path("products/<int:pk>/delete/", views.product_delete),
+    path("products/", views.product_list, name="product_list"),
+    path("products/create/", views.product_create, name="product_create"),
+    path("products/<int:pk>/edit/", views.product_edit, name="product_edit"),
+    path("products/<int:pk>/delete/", views.product_delete, name="product_delete"),
 ]
