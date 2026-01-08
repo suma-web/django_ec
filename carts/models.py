e91f8a33d6dd6bac21c51cb51844b8af204e70a2
sample tarou <sample@example.com>
2026-01-08 22:19:07 +0900

    carts/models.pyに追記

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, related_name="items", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price_at_add = models.IntegerField()
