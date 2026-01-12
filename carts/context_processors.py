08f5d7dbe0a4a163e10579707ec4a0323f4565fe
sample tarou <sample@example.com>
2026-01-12 13:04:16 +0900

    carts/cart.pyの削除、carts/context_processors.pyの削除

--- carts/context_processors.py
+++
@@ -1,5 +0,0 @@
-def cart_item_count(request):
-    cart = request.session.get("cart", {})
-    total_quantity = sum(item.get("quantity", 0) for item in cart.values())
-    
-    return {"cart_item_count": total_quantity}
