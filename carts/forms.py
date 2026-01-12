c42324bb74562184b51a51d72c298d14bc4d40af
sample tarou <sample@example.com>
2026-01-12 17:06:40 +0900

    carts/forms.pyにwidgets項目を追加

--- carts/forms.py
+++ carts/forms.py
@@ -9,3 +9,18 @@ class CheckoutForm(forms.ModelForm):
             "address", "address2", "country", "state", "zip_code",
             "card_name", "card_number", "card_expiration", "card_cvv",
         ]
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "address": forms.TextInput(attrs={"class": "form-control"}),
+            "address2": forms.TextInput(attrs={"class": "form-control"}),
+            "country": forms.Select(attrs={"class": "form-select"}),
+            "state": forms.Select(attrs={"class": "form-select"}),
+            "zip_code": forms.TextInput(attrs={"class": "form-control"}),
+            "code_name": forms.TextInput(attrs={"class": "form-control"}),
+            "code_number": forms.TextInput(attrs={"class": "form-control"}),
+            "code_expiration": forms.TextInput(attrs={"class": "form-control"}),
+            "code_cvv": forms.TextInput(attrs={"class": "form-control"}),
+        }
