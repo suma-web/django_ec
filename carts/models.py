78eaeab30577a94edc99af18996c6f94f4409b30
sample tarou <sample@example.com>
2026-01-12 15:16:21 +0900

    carts/models.pyにclass Order と OderItem を追加

--- carts/models.py
+++ carts/models.py
@@ -20,3 +20,40 @@ class CartItem(models.Model):
     @property
     def total_price(self):
         return self.price_at_add * self.quantity
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(blank=True)
+
+    address = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
+
+    # Payment（※学習用）
+    card_name = models.CharField(max_length=100)
+    card_number = models.CharField(max_length=30)
+    card_expiration = models.CharField(max_length=10)
+    card_cvv = models.CharField(max_length=10)
+
+    total_price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        related_name="items",
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey("products.Product", on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    price = models.IntegerField()
+
+    def subtotal(self):
+        return self.quantity * self.price
