7144a49e2e94e4d483db214127d6dc84193efaf4
sample tarou <sample@example.com>
2026-01-14 16:04:51 +0900

    config/templates/carts/cart_view.htmlにname属性を追加

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.cart_view, name="cart_view"),
     path("add/<int:product_id>/", views.cart_add, name="cart_add"),
     path("remove/<int:product_id>/", views.cart_remove, name="cart_remove"),
-    path("checkout/", views.checkout, name="checkout")
+    path("checkout/", views.checkout, name="checkout"),
 ]
