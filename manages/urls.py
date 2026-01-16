a6654e64d253f32f18bec2e780ed89694c06ba65
sample tarou <sample@example.com>
2026-01-17 01:05:46 +0900

    manages/urls.py内にorder_listのpathを移行

--- manages/urls.py
+++ manages/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("products/create/", views.product_create, name="product_create"),
     path("products/<int:pk>/edit/", views.product_edit, name="product_edit"),
     path("products/<int:pk>/delete/", views.product_delete, name="product_delete"),
-    path("products/orders/", views.order_list, name="order_list"),
-    path("products/orders/<int:order_id>/", views.order_detail, name="order_detail"),
+    path("orders/", views.order_list, name="order_list"),
+    path("orders/<int:order_id>/", views.order_detail, name="order_detail"),
 ]
