4ef88d7b2057b9f6da20fdbce1539b8ff5ae252f
sample tarou <sample@example.com>
2026-01-19 15:43:22 +0900

    プロモーションコードを適用するためのurlを設定(carts/urls.py)

--- carts/urls.py
+++ carts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("checkout/", views.checkout, name="checkout"),
     path("orders/", views.order_list, name="order_list"), 
     path("orders/<int:order_id>/", views.order_detail, name="order_detail"),
+    path("promo/", views.apply_promo_code, name="apply_promo_code"),
 ]
