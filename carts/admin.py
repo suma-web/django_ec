fb21f1f399cfe77f1922ac004df063459822dfdb
sample tarou <sample@example.com>
2026-01-19 16:06:14 +0900

    プロモーションコードモデルをadminに反映されるためにクラスオブジェクトを作成(carts/admin.py)

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, PromotionCode
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+@admin.register(PromotionCode)
+class PromotionCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "discount_amount",
+        "is_used",
+        "created_at",
+    )
+    list_filter = ("is_used",)
+    search_fields = ("code",)
